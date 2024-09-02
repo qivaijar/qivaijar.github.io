3b78e617a4bbef0097dbbfbe1d70800584ec2468
qivaijar <qivaijar@gmail.com>
2024-09-02 14:10:09 +0000

    update repo name

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -3,12 +3,12 @@ import { getSkills } from './skills';
 
 export const title = 'Home';
 
-export const name = 'Name';
+export const name = 'Rizki Rivai';
 
-export const lastName = 'LastName';
+export const lastName = 'Ginanjar';
 
 export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+	'Hey there. I\'m a product';
 
 export const links: Array<{ platform: Platform; link: string }> = [
 	{ platform: Platform.GitHub, link: 'https://github.com/' },
