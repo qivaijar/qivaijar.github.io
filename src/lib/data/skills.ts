e8e0ddff53f8811035f59c49b7e71e2ca1585206
qivaijar <qivaijar@gmail.com>
2024-09-15 14:53:30 +0000

    add an explanation of a relevant skill

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -13,6 +13,7 @@ import docker from '../md/docker.md?raw';
 import numpy from '../md/numpy.md?raw';
 import pandas from '../md/pandas.md?raw';
 import sklearn from '../md/sklearn.md?raw';
+import matplotlib from '../md/matplotlib.md?raw';
 import { omit, type StringWithAutoComplete } from '@riadh-adrani/utils';
 
 const defineSkillCategory = <S extends string>(data: SkillCategory<S>): SkillCategory<S> => data;
@@ -132,8 +133,7 @@ export const items = [
 	defineSkill({
 		slug: 'matplotlib',
 		color: '#EED202',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		description: matplotlib,
 		logo: Assets.Matplotlib,
 		name: 'Matplotlib',
 		category: 'library'
