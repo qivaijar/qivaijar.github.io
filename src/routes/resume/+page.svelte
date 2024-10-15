55042eab36390db21e8d46853e65d4ba2b0afac3
qivaijar <qivaijar@gmail.com>
2024-10-15 15:53:49 +0000

    make PdfViewer component SSR compatible

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,13 @@
 <script>
 	import { data, title } from '@data/resume';
-	import  PdfViewer from "svelte-pdf";
+	import { onMount } from "svelte";
+  	let PdfViewer;
+
+  	onMount(async () => {
+    const module = await import("svelte-pdf");
+    PdfViewer = module.default;
+  	});
+
 	import Chip from '$lib/components/Chip/Chip.svelte';
 	import CommonPage from '$lib/components/CommonPage.svelte';
 </script>
@@ -11,13 +18,22 @@
 			<!-- <a href={data} download>
 				<Chip size={'1.25em'}>Download</Chip>
 			</a> -->
-			<PdfViewer 
+			<!-- <PdfViewer 
+			url='src/lib/data/{data}' 
+			showBorder={false}
+			pageNum = {1}
+			showButtons={["navigation", "zoom", 'print', "download"]}
+			downloadFileName="rizki_rivai_ginanjar_resume.pdf"
+			/> -->
+
+			<svelte:component this={PdfViewer}
 			url='src/lib/data/{data}' 
 			showBorder={false}
 			pageNum = {1}
 			showButtons={["navigation", "zoom", 'print', "download"]}
 			downloadFileName="rizki_rivai_ginanjar_resume.pdf"
 			/>
+
 		{:else}
 			<Chip>Ooops! no CV at the moment.</Chip>
 		{/if}
