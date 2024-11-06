b00214d0eb55e5f78bf256fe010f9fa69d588ba5
qivaijar <qivaijar@gmail.com>
2024-11-06 14:03:23 +0000

    Updates

--- _app/immutable/chunks/paths.7465ba6d.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1eatlf6)==null?void 0:s.base)??"/rizki-svelte-portfolio";var e;const a=((e=globalThis.__sveltekit_1eatlf6)==null?void 0:e.assets)??t;export{a,t as b};
