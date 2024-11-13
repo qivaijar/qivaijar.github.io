fdf23a5671515ea09e1017f20e02cc9fcd0fd10b
qivaijar <qivaijar@gmail.com>
2024-11-13 14:35:05 +0000

    add assets to the project

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,6 +1,7 @@
 import Assets from './assets';
 import { getSkills } from './skills';
 import type { Project } from '../types';
+import { base } from '$app/paths';
 
 export const items: Array<Project> = [
 	{
@@ -21,42 +22,49 @@ export const items: Array<Project> = [
 	{
 		slug: 'rizki-portfolio-svelte',
 		color: '#0AFFFF',
-		description: '',
+		description: `I have developed this portfolio website to exhibit my skills and experiences. 
+					It was implemented using Svelte, utilizing a template sourced from [this repository](https://github.com/RiadhAdrani/slick-portfolio-svelte).
+					The site is hosted on GitHub Pages as a static webpage.
+					Here, you can explore the technical stacks and skills I possess, view products and projects I have worked on, explore my professional experiences, educational background, and access my most recent curriculum vitae.
+					  `,
 		shortDescription: 'My personal portfolio website.',
-		links: [{ to: 'https://github.com/qivaijar/rizki-svelte-portfolio', label: 'GitHub' }],
+		links: [
+			{ to: 'https://github.com/qivaijar/rizki-svelte-portfolio', label: 'GitHub' },
+			{ to: 'https://qivaijar.github.io/rizki-svelte-portfolio/', label: 'Portfolio Page' },
+		],
 		logo: Assets.Qivaijar,
 		name: 'My Portfolio Website',
 		period: {
 			from: new Date('2024-09')
 		},
-		skills: getSkills('svelte'),
-		type: 'Portfolio Website'
-		// screenshots: [
-		// 	{
-		// 		label: 'screen 1',
-		// 		src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-		// 	},
-		// 	{
-		// 		label: '2',
-		// 		src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-		// 	},
-		// 	{
-		// 		label: '3',
-		// 		src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-		// 	},
-		// 	{
-		// 		label: '4',
-		// 		src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-		// 	},
-		// 	{
-		// 		label: '5',
-		// 		src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-		// 	},
-		// 	{
-		// 		label: '6',
-		// 		src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-		// 	}
-		// ]
+		skills: getSkills('svelte', 'git'),
+		type: 'Portfolio Website',
+		screenshots: [
+			{
+				label: 'Home Page',
+				src: "portfolio_website_home.png"
+			},
+			{
+				label: 'Skills Page',
+				src: "skills.png"
+			},
+			{
+				label: 'Products / Projects Page',
+				src: "projects.png"
+			},
+			{
+				label: 'Experience Page',
+				src: "experience.png"
+			},
+			{
+				label: 'Education Page',
+				src: "education.png"
+			},
+			{
+				label: 'Resume Page',
+				src: "resume.png"
+			},
+		]
 	}
 ];
 
